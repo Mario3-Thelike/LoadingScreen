86531570fec7342eabad2cb2151028f9698f7f43
Mario3-Thelike <74585097+Mario3-Thelike@users.noreply.github.com>
2021-02-18 16:37:07 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,34 @@
+fx_version 'bodacious'
+game 'gta5'
+
+author 'MixZira'
+contact 'E-mail: mixzira@outlook.com.br - Discord: MixZira#0001'
+version '1.0.0'
+
+client_scripts {
+    'hansolo/*'
+}
+
+files {
+    --[ NUI ]---------------------------
+    'nui/*',
+    'nui/*',
+    'nui/*',
+    --[ Músicas ]-----------------------
+    'nui/**/*',
+    'nui/**/*',
+    'nui/**/*',
+    'nui/**/*',
+    'nui/**/*',
+    --[ Imagens ]-----------------------
+    'nui/images/palamande.png',
+    'nui/images/rechinii.png',
+    'nui/images/nanetop10.png',
+    'nui/images/ianduble.png',
+    'nui/images/Azideliric.png',
+    'nui/images/daladumblamulti.png'
+}
+
+loadscreen 'nui/darkside.html'
+loadscreen_manual_shutdown "yes"
+
